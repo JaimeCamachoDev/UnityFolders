389050a7616221c6d3269dc98933de9ddb38654b
Jaime Camacho <jaime.camacho.dev@gmail.com>
2025-07-02 00:36:55 +0200

    Delete Packages/com.jaimecamacho.unityfolders/Editor directory

--- Packages/com.jaimecamacho.unityfolders/Editor/Scripts/Info/AssetInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace UnityFolders
-{
-    internal static class AssetInfo
-    {
-        public const string NAME = "UnityFolders";
-        public const string VERSION = "1.0.0";
-        public const string HELP_URL = "https://github.com/JaimeCamachoDev/UnityFolders/blob/main/README.md";
-    }
-}
