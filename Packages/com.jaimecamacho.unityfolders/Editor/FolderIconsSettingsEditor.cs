9f70a8047a0cbbebed9c609993a0f26b049d797c
Jaime Camacho <jaime.camacho.dev@gmail.com>
2025-07-03 19:18:30 +0200

    update

--- Packages/com.jaimecamacho.unityfolders/Editor/FolderIconsSettingsEditor.cs
+++ Packages/com.jaimecamacho.unityfolders/Editor/FolderIconsSettingsEditor.cs
@@ -38,7 +38,14 @@ public class FolderIconsSettingsEditor : Editor
             EditorGUI.LabelField(rect, "Folder Icon Rules");
         };
 
-        list.elementHeightCallback = index => EditorGUIUtility.singleLineHeight * 8 + 52;
+        list.elementHeightCallback = index =>
+        {
+            float lineHeight = EditorGUIUtility.singleLineHeight + 4;
+            float height = lineHeight * 7;
+            if (((FolderIconsSettings)target).showPreview)
+                height += lineHeight * 2.5f + 4;
+            return height;
+        };
 
         list.drawElementCallback = (rect, index, isActive, isFocused) =>
         {
@@ -46,50 +53,83 @@ public class FolderIconsSettingsEditor : Editor
             float lineHeight = EditorGUIUtility.singleLineHeight + 4;
             float y = rect.y;
 
+            var backgroundProp = element.FindPropertyRelative("background");
+            var iconSmallProp = element.FindPropertyRelative("iconSmall");
+            var iconLargeProp = element.FindPropertyRelative("iconLarge");
+            var overlayProp = element.FindPropertyRelative("overlayIcon");
+            var previewSmallProp = element.FindPropertyRelative("previewSmall");
+            var previewLargeProp = element.FindPropertyRelative("previewLarge");
+
+            bool updatePreview = false;
+
             EditorGUI.PropertyField(new Rect(rect.x, y, rect.width, lineHeight), element.FindPropertyRelative("match"), new GUIContent("Match Path"));
             y += lineHeight;
             EditorGUI.PropertyField(new Rect(rect.x, y, rect.width, lineHeight), element.FindPropertyRelative("matchType"), new GUIContent("Match Type"));
             y += lineHeight;
-            EditorGUI.PropertyField(new Rect(rect.x, y, rect.width, lineHeight), element.FindPropertyRelative("background"), new GUIContent("Tint Color"));
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.PropertyField(new Rect(rect.x, y, rect.width, lineHeight), backgroundProp, new GUIContent("Tint Color"));
+            if (EditorGUI.EndChangeCheck()) updatePreview = true;
             y += lineHeight;
-            EditorGUI.PropertyField(new Rect(rect.x, y, rect.width, lineHeight), element.FindPropertyRelative("iconSmall"), new GUIContent("Icon Small"));
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.PropertyField(new Rect(rect.x, y, rect.width, lineHeight), iconSmallProp, new GUIContent("Icon Small"));
+            if (EditorGUI.EndChangeCheck()) updatePreview = true;
             y += lineHeight;
-            EditorGUI.PropertyField(new Rect(rect.x, y, rect.width, lineHeight), element.FindPropertyRelative("iconLarge"), new GUIContent("Icon Large"));
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.PropertyField(new Rect(rect.x, y, rect.width, lineHeight), iconLargeProp, new GUIContent("Icon Large"));
+            if (EditorGUI.EndChangeCheck()) updatePreview = true;
             y += lineHeight;
-            EditorGUI.PropertyField(new Rect(rect.x, y, rect.width, lineHeight), element.FindPropertyRelative("overlayIcon"), new GUIContent("Overlay Icon"));
+            EditorGUI.BeginChangeCheck();
+            EditorGUI.PropertyField(new Rect(rect.x, y, rect.width, lineHeight), overlayProp, new GUIContent("Overlay Icon"));
+            if (EditorGUI.EndChangeCheck()) updatePreview = true;
             y += lineHeight;
 
-            // Previews
+            if (updatePreview)
+            {
+                Texture2D small = iconSmallProp.objectReferenceValue as Texture2D;
+                Texture2D large = iconLargeProp.objectReferenceValue as Texture2D;
+                Texture2D overlay = overlayProp.objectReferenceValue as Texture2D;
+                Color bg = backgroundProp.colorValue;
+
+                previewSmallProp.objectReferenceValue = small != null ? FolderIconRecolorUtility.Recolor(small, bg) : null;
+                previewLargeProp.objectReferenceValue = large != null ? FolderIconRecolorUtility.Recolor(large, bg, overlay) : null;
+            }
+
             var settings = (FolderIconsSettings)target;
-            var rule = settings.rules[index];
-            float previewSize = lineHeight * 2.5f;
-            Rect smallRect = new Rect(rect.x, y, previewSize, previewSize);
-            Rect largeRect = new Rect(rect.x + previewSize + 4, y, previewSize, previewSize);
-
-            if (rule.iconSmall != null)
+            if (settings.showPreview)
             {
-                if (rule.background.a > 0.01f)
-                    GUI.DrawTexture(smallRect, GetGradient(rule.background), ScaleMode.StretchToFill);
-                var tex = FolderIconRecolorUtility.Recolor(rule.iconSmall, rule.background);
-                GUI.DrawTexture(smallRect, tex, ScaleMode.ScaleToFit, true);
-            }
+                float previewSize = lineHeight * 2.5f;
+                Rect smallRect = new Rect(rect.x, y, previewSize, previewSize);
+                Rect largeRect = new Rect(rect.x + previewSize + 4, y, previewSize, previewSize);
 
-            if (rule.iconLarge != null)
-            {
-                if (rule.background.a > 0.01f)
-                    GUI.DrawTexture(largeRect, GetGradient(rule.background), ScaleMode.StretchToFill);
-                var tex = FolderIconRecolorUtility.Recolor(rule.iconLarge, rule.background, rule.overlayIcon);
-                GUI.DrawTexture(largeRect, tex, ScaleMode.ScaleToFit, true);
-            }
+                Texture2D smallTex = previewSmallProp.objectReferenceValue as Texture2D;
+                Texture2D largeTex = previewLargeProp.objectReferenceValue as Texture2D;
 
-            y += previewSize + 4;
+                if (smallTex != null)
+                {
+                    if (backgroundProp.colorValue.a > 0.01f)
+                        GUI.DrawTexture(smallRect, GetGradient(backgroundProp.colorValue), ScaleMode.StretchToFill);
+                    GUI.DrawTexture(smallRect, smallTex, ScaleMode.ScaleToFit, true);
+                }
+
+                if (largeTex != null)
+                {
+                    if (backgroundProp.colorValue.a > 0.01f)
+                        GUI.DrawTexture(largeRect, GetGradient(backgroundProp.colorValue), ScaleMode.StretchToFill);
+                    GUI.DrawTexture(largeRect, largeTex, ScaleMode.ScaleToFit, true);
+                }
+
+                y += previewSize + 4;
+            }
 
             if (GUI.Button(new Rect(rect.x, y, rect.width, lineHeight), "Apply Color & Generate Icons"))
             {
-                if (rule.iconSmall != null)
-                    rule.iconSmall = FolderIconRecolorUtility.RecolorAndSave(rule.iconSmall, rule.background, rule.match + "_Small");
-                if (rule.iconLarge != null)
-                    rule.iconLarge = FolderIconRecolorUtility.RecolorAndSave(rule.iconLarge, rule.background, rule.match + "_Large", rule.overlayIcon);
+                if (iconSmallProp.objectReferenceValue != null)
+                    iconSmallProp.objectReferenceValue = FolderIconRecolorUtility.RecolorAndSave((Texture2D)iconSmallProp.objectReferenceValue, backgroundProp.colorValue, element.FindPropertyRelative("match").stringValue + "_Small");
+                if (iconLargeProp.objectReferenceValue != null)
+                    iconLargeProp.objectReferenceValue = FolderIconRecolorUtility.RecolorAndSave((Texture2D)iconLargeProp.objectReferenceValue, backgroundProp.colorValue, element.FindPropertyRelative("match").stringValue + "_Large", overlayProp.objectReferenceValue as Texture2D);
+
+                previewSmallProp.objectReferenceValue = iconSmallProp.objectReferenceValue as Texture2D;
+                previewLargeProp.objectReferenceValue = iconLargeProp.objectReferenceValue as Texture2D;
             }
         };
     }
@@ -97,6 +137,7 @@ public class FolderIconsSettingsEditor : Editor
     public override void OnInspectorGUI()
     {
         serializedObject.Update();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("showPreview"));
         list.DoLayoutList();
         serializedObject.ApplyModifiedProperties();
     }
